e8398e19f0e1ea7098c5d1c924c9a79259df8511
amal syazwani <21FTT1291@student.pb.edu.bn>
2023-04-10 22:28:33 +0800

    appvue

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,33 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
+ 
+    <!-- <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+
+  
+
+<div class="grid grid-rows-3 grid-flow-col gap-4 max-md">
+
+  <div class="row-span-3 "><img src="mypic.jpeg" width="200" height="100" alt=""></div>
+
+  <div class="col-span-2 underline"><h1>About me</h1></div>
+
+  <div class="row-span-2 col-span-2 bg-[#083344]" >
+    <p class="text-white ">Hellow! My Name is Syazwani also known as Wani. 
+    I am a student from Politeknik Brunei currently taking Web development.
+    I would describe myself as an introvert person.
+    My MBTI is INFJ.
+  </p>
+  </div>
+</div>
+
 </template>
 
 <style scoped>
